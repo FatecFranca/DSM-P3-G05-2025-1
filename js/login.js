10655ac61b37658bb92305568014d6289bcf75fd
unknown <tinc3lle@gmail.com>
2025-04-22 20:43:10 -0300

    Botão dropdown no login para selecionar modo de jogo e melhorias no login

--- js/login.js
+++ js/login.js
@@ -1,36 +1,32 @@
+const select = document.querySelector('.login__select');
 const input = document.querySelector('.login__input');
 const button = document.querySelector('.login__button');
-const buttonnaruto = document.querySelector('.loginnaruto__button');
 const form = document.querySelector('.login-form');
 
-const validateInput = ({ target }) => {
-  if (target.value.length > 2) {
-    button.removeAttribute('disabled');
-    buttonnaruto.removeAttribute('disabled');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const playerName = input.value.trim();
+  const gameMode = select.value;
+  
+  if (!playerName || !gameMode) {
+    alert('Por favor, preencha seu nome e selecione um modo de jogo.');
     return;
   }
+  
+  localStorage.setItem('player', playerName);
+  
+  if (gameMode === 'rickmorty') {
+    window.location.href = './pages/game.html';
+  } else if (gameMode === 'naruto') {
+    window.location.href = './pages/gamenaruto.html';
+  }
+});
 
-  button.setAttribute('disabled', '');
-  buttonnaruto.setAttribute('disabled', '');
-};
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  localStorage.setItem('player', input.value);
-  window.location = 'pages/game.html';
-};
-
-const handleSubmitNaruto = (event) => {
-  event.preventDefault();
-
-  localStorage.setItem('player', input.value);
-  window.location = 'pages/gamenaruto.html';
-};
-
-// Adicione eventos de clique para cada botão separadamente
-button.addEventListener('click', handleSubmit);
-buttonnaruto.addEventListener('click', handleSubmitNaruto);
-
-// Validação do input
-input.addEventListener('input', validateInput);
+input.addEventListener('input', ({ target }) => {
+  if (target.value.length > 2) {
+    button.removeAttribute('disabled');
+  } else {
+    button.setAttribute('disabled', '');
+  }
+});
