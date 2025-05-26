0df37af6363d161f96e6dde35e8108f4bf4bff46
João Vitor Nicolau <tinc3lle@gmail.com>
2025-05-26 11:35:58 -0300

    Front end | Remoção de partes desnecessarias.

--- js/login.js
+++ js/login.js
@@ -1,39 +1,3 @@
-// const select = document.querySelector('.login__select');
-// const input = document.querySelector('.login__input');
-// const button = document.querySelector('.login__button');
-// const form = document.querySelector('.login-form');
-
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-
-//   const playerName = input.value.trim();
-//   const gameMode = select.value;
-
-//   if (!playerName || !gameMode) {
-//     alert('Por favor, preencha seu nome e selecione um modo de jogo.');
-//     return;
-//   }
-
-//   localStorage.setItem('player', playerName);
-
-//   if (gameMode === 'rickmorty') {
-//     window.location.href = './pages/game.html';
-//   } else if (gameMode === 'naruto') {
-//     window.location.href = './pages/gamenaruto.html';
-//   }
-
-//   localStorage.setItem('difficulty', selectDifficulty.value);
-//   window.location.href = './pages/game.html';
-// });
-
-// input.addEventListener('input', ({ target }) => {
-//   if (target.value.length > 2) {
-//     button.removeAttribute('disabled');
-//   } else {
-//     button.setAttribute('disabled', '');
-//   }
-// });
-
 const selectMode = document.querySelector(".login__select");
 const selectDifficulty = document.querySelector(".difficulty-select");
 const input = document.querySelector(".login__input");
